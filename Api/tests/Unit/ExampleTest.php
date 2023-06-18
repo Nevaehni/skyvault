cd7f1f93bb333987cb453c3f30cedb964e46ea93
Sebahattin Erzincan <sebahattin.erzincan@gmail.com>
2023-06-18 23:25:27 +0200

    Testing ci cd

---
+++ Api/tests/Unit/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample()
+    {
+        $calculator = 0;
+        $result = $calculator + 5;
+        $this->assertEquals(5, $result);
+    }
+}
